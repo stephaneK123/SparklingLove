1f8e1a7bc77bab63325cf2e8d9b80fd018ffdf94
Stephane K <Swar.kung@gmail.com>
2022-07-07 08:04:04 -0400

    init
    init

---
+++ app/src/main/java/com/example/sparklinglove/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sparklinglove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val hello:String = "Hiii"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
